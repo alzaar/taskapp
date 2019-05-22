cb8baf38e06427452d13af848242ce5c29457ad0
Ali Zahir <alizahir@live.com>
2019-05-22 14:34:08 -0400

    Edit Middleware

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 //Router Setup
 const express = require('express');
 const router = express();
+//Secondary Req.
+const validateEmail = require('../HelperFunctions/emailValidator');
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 //Routes
 router.get('/login', (req, res) => {
   res.render('login');
@@ -11,7 +15,62 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 router.post('/register', (req, res) => {
-  console.log(req.body);
-  res.send('hello');
+  const { name, email, password, password2 } = req.body;
+  const errors = [];
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in the fields' })
+  }
+  if (password !== password2) {
+    errors.push({ msg: 'Passwords do not match' })
+  }
+  if (password.length < 5) {
+    errors.push({ msg: 'Incorrect password length' })
+  }
+  // if (validateEmail(email)) {
+  //   errors.push({ msg: 'Invalid Email' });
+  // }
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  } else {
+      User.findOne(user => {
+        if (user) {
+          errors.push({ msg: 'User already exists' });
+          res.render('register', {
+            errros,
+            name,
+            email,
+            password,
+            password2
+          })
+        } else {
+          const newUser = new User({
+            name,
+            email,
+            password
+          });
+          //Hashing
+          bcrypt.genSalt(10, (err, salt) =>
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              //Hash assig.
+              newUser.password = hash;
+              //Save the user
+              newUser.save()
+                .then(user => {
+                  req.flash('success_msg', 'Logged In!')
+                  res.redirect('/users/login');
+                })
+                .catch(err => console.log(err));
+            })
+          )
+        }
+      })
+  }
 })
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = 3000;
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const db = require('./config/keys').MongoURI;
+const flash = require('connect-flash');
+const session = require('express-session');
 //Router
 const mainRoute = require('./routes/index');
 const user = require('./routes/user');
@@ -16,8 +18,20 @@ mongoose.connect(db, { useNewUrlParser: true })
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
 app.set('view engine', 'ejs');
 app.use('/', mainRoute);
 app.use('/users', user);
+//Global Variables
+app.use((req, res, next) => {
+  req.locals.success_msg = req.flash('success_msg');
+  req.locals.error_msg = req.flash('error_msg');
+  next();
+})
 //For Starting the server
 app.listen(PORT, () => { console.log(`Listening at ${PORT}`) });
