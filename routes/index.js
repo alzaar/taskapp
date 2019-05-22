7617f59c0de16270925e8cfe6f7132dad32894cd
Ali Zahir <alizahir@live.com>
2019-05-22 16:23:57 -0400

    Finish Authentication

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@
 const express = require('express');
 const router = express();
 const user = require('./user');
+const { isAuth } = require('../config/auth');
 //Routes
 router.get('/', (req, res) => {
   res.render('welcome');
 });
+
+router.get('/dashboard', isAuth, (req, res) => {
+  res.render('dashboard', {
+    user: req.user
+  })
+})
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -83,4 +83,11 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
+
+//Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'Logged Out!');
+  res.redirect('/users/login');
+})
 module.exports = router;
