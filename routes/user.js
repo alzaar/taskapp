0ed0b282aae52c674ab0546cff75f25c121a1407
Ali Zahir <alizahir@live.com>
2019-05-22 00:51:53 -0400

    Add mongo config

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,13 @@ const router = express();
 router.get('/login', (req, res) => {
   res.render('login');
 });
+//@desc Test
+//GET /register
 router.get('/register', (req, res) => {
   res.render('register');
 });
-
+router.post('/register', (req, res) => {
+  console.log(req.body);
+  res.send('hello');
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const db = require('./config/keys').MongoURI;
 //Router
 const mainRoute = require('./routes/index');
 const user = require('./routes/user');
+//DB Setup
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.log(err));
+
 //Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.use('/', mainRoute);
 app.use('/users', user);
-
 //For Starting the server
 app.listen(PORT, () => { console.log(`Listening at ${PORT}`) });
